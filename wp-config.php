1024c3b8f4c9a76e90cc6c314a3469486d0e48d9
widiani <52686733+widiani@users.noreply.github.com>
2019-09-12 11:29:20 +0700

    wp-config menggunakan database rds

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'web' );
+define( 'DB_NAME', 'dbwordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'widi' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'jaringan' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'dbwordpress.cf4kbdfuw9lt.us-east-2.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
